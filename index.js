87ae3fdfc5bd3d9a8a80d5d96bfca750055d5f59
CarlTanilon <152235675+CarlTanilon@users.noreply.github.com>
2025-09-30 07:15:22 +0800

    added routes in the index file

--- index.js
+++ index.js
@@ -10,62 +10,63 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/', async (req, res) => {
+    // Include all properties we want to retrieve
+    const properties = ['firstname', 'email', 'phone', 'createdate'];
+    const crmUrl = `https://api.hubspot.com/crm/v3/objects/contacts?properties=${properties.join(',')}`;
+    
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
-    }
+    };
+    
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const response = await axios.get(crmUrl, { headers });
+        const contacts = response.data.results || [];
+        res.render('homepage', { 
+            title: 'CRM Contacts | Home',
+            contacts: contacts
+        });
     } catch (error) {
-        console.error(error);
+        console.error('Error fetching contacts:', error);
+        res.status(500).render('error', { 
+            message: 'Error loading contacts',
+            error: process.env.NODE_ENV === 'development' ? error : {}
+        });
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
+app.post('/update-cobj', async (req, res) => {
+    const { name, email, phone } = req.body;
+    
+    const newContact = {
         properties: {
-            "favorite_book": req.body.newVal
+            "firstname": name,
+            "email": email,
+            "phone": phone
         }
-    }
+    };
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const url = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        await axios.post(url, newContact, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error creating contact:', error);
+        res.status(500).send('Error creating contact');
     }
-
 });
-*/
 
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { 
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+});
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
